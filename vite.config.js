1b9007165ec8ff5c2ef2036f423c0f9e1a11cc56
Gerald Choqueux <gerald.choqueux@ign.fr>
2025-02-05 16:12:03 +0100

    chore: remove root src
    fix(deploy): old version package github action
    
    * Always build the best working development (dev/lint/debug/build and deploy)
    * Always re-use the nest environment -> write a environment on lastig github
    * try to uses last versions packages because the update can be impossible

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,26 @@
-import { defineConfig } from "vite"
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-    base : '/3D-Viewer/',
-    build:{
-      rollupOptions:{
-        external:['regenerator-runtime/runtime']
-      }
+  base: "/Building-3D-Editor/",
+  build: {
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['regenerator-runtime/runtime'],
+    },
+  },
+  server: {
+    headers: {
+      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline';"
     }
-  })
+  },
+  preview: {
+    headers: {
+      "Content-Security-Policy": "default-src 'self'; script-src 'self' 'unsafe-inline';"
+    }
+  }
+})
